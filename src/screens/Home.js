6f05fb0d3adf91eface801b971b2b7c31ae69cfa
kfth <nieuwleven33@gmail.com>
2020-05-09 07:43:09 +0200

    feat: Deposit input added to Home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,6 +5,7 @@ import Input from '../components/Input';
 
 const Home = (props) => {
   const [vehiclePrice, setVehiclePrice] = useState('');
+  const [deposit, setDeposit] = useState('');
 
   return (
     <View>
@@ -14,9 +15,15 @@ const Home = (props) => {
         onChangeHandler={setVehiclePrice}
         defaultValue={vehiclePrice}
       />
+      <Input
+        title='Deposit'
+        placeholder='Enter deposit amount'
+        onChangeHandler={setDeposit}
+        defaultValue={deposit}
+      />
       <Button
         onPress={() => {
-          console.log(vehiclePrice);
+          console.log(vehiclePrice, deposit);
         }}
         title='Payment Schedule'
       />
