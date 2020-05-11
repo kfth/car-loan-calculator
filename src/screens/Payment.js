4264676d78045b13d4f4b1c1642194d54db1c019
kfth <nieuwleven33@gmail.com>
2020-05-11 06:14:04 +0200

    feat: Payment screen layout

--- src/screens/Payment.js
+++ src/screens/Payment.js
@@ -1,13 +1,49 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+
+import Card from '../components/InputContainer';
 
 const Payment = (props) => {
+  const { deposit, loan, months, payments } = JSON.parse(props.route.params);
   return (
-    <View>
-      <Text>Payment</Text>
-      <Text>{props.route.params}</Text>
+    <View style={styles.container}>
+      <Card style={styles.header}></Card>
+      <View style={styles.listContainer}></View>
+      <View style={styles.footer}></View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative',
+  },
+  header: {
+    height: 80,
+    backgroundColor: '#99CB38',
+    borderWidth: 0,
+  },
+  listContainer: {
+    flex: 0.88,
+    paddingBottom: 60,
+    marginBottom: -60,
+  },
+  footer: {
+    flex: 0.12,
+    flexDirection: 'row',
+    backgroundColor: '#99CB38',
+    bottom: 0,
+    padding: 0,
+  },
+  backButton: {
+    flexGrow: 1,
+    margin: 0,
+  },
+  carsButton: {
+    flexGrow: 3,
+    margin: 0,
+  },
+});
 
 export default Payment;
